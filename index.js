ee9ebbb2266cb34de198d7aa9be04615d82bce02
kai021998 <kai.han@balliol.ox.ac.uk>
2018-08-21 14:18:53 -0400

    started front end stuff

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
+  fetch("http://localhost:3000/api/v1/sentences")
+    .then(text => text.json())
+    .then(parsed => runApplication(parsed))
+
+  function runApplication(sentenceArray) {
+    const sentence = getRandomSentence(sentenceArray)
+    displaySentence(sentence)
+
+  }
+  function getRandomSentence(info) {
+    return info[Math.floor(Math.random()*info.length)];
+  }
+
+  function displaySentence(sentence) {
+    const sentenceWordArray = sentence.content.split(" ")
+    const sentenceDiv = document.getElementById("sentence")
+    let counter = 0
+    sentenceWordArray.forEach(word => {
+      const wordDiv = document.createElement("div")
+      wordDiv.setAttribute("id", `word-${++counter}`)
+      wordDiv.setAttribute("class", "word-box")
+      wordDiv.innerText = word
+      sentenceDiv.appendChild(wordDiv)
+    })
+  }
 
 
-
-}
+})
